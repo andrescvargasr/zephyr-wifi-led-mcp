7e64471b175e7df9e174b762df5007a29308b02c
andresc.vargasr@gmail.com <andresc.vargasr@gmail.com>
2026-07-22 15:46:33 +0200

    feat: initialize project version and add thread management boilerplate

---
+++ src/_start_threads.c
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2026 Andrés Camilo Román Vargas
+ * @file _start_threads.c
+ * @author Andres C. Vargas R. (camilo.vargas@technaid.com gh: @andrescvargasr)
+ * @brief
+ * @version 0.1
+ * @date 2024-07-24
+ *
+ * Define and initialize the Threads and Workqueues for the project.
+ * SPDX-License-Identifier: MIT
+ */
+
+// Parameters
+#include "params.h"
+
+// Threads
+#ifdef THD_LED
+#include "thd_led.h"
+#endif // THD_LED
+
+#define THD_STACKSIZE 512 // Should always be a power of two.
+#define THREAD_LED_PRIORITY K_PRIO_PREEMPT(2)
+
+/******************************************************************************/
+/*                          Zephyr Workqueue Table	                          */
+/******************************************************************************/
+// #ifdef THD_WQ
+// K_THREAD_STACK_DEFINE(my_stack_area, WORQ_THREAD_STACK_SIZE * 2); // Check first thd_wq.c
+// #endif
+
+/******************************************************************************/
+/*                          Zephyr Message Queue	                          */
+/******************************************************************************/
+// K_FIFO_DEFINE(my_fifo);
+
+/******************************************************************************/
+/*                          Zephyr Multithreading Table                       */
+/******************************************************************************/
+#ifdef THD_LED
+K_THREAD_DEFINE(thd_led, THD_STACKSIZE, thread_led, NULL, NULL, NULL,
+				THREAD_LED_PRIORITY, 0, 0);
+#endif // End THD_LED
